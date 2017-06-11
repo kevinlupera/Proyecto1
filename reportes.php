9fa3f317172d9929d1e3d5a7b1ae1f966f49573b
lupera13k <kevinlupera1997@outlook.com>
2017-06-11 17:40:31 -0500

    Merge master into NN
    Conflicts:
    	codigos/footer.php
    	login.php
    	loginController.php

--- reportes.php
+++ reportes.php
@@ -16,7 +16,7 @@ and open the template in the editor.
         <div class="contenedor">
             <section class="reporte-dia">
                 <h2>Reporte del Día</h2>
-                <button type="button" onmouseover="adicionarRepor(1)" href="#">Agregar un reporte</button>
+                <button type="button" onclick="adicionarRepor(1)>Agregar un reporte</button>
                 </br>    
                 <button type="button" onclick="quitarRepor()">Quitar un reporte</button>
                 <button type="button" onclick="aparecerRepor()">Mostrar el reporte</button>
