2230c39c64794f61f5ef459811838dc62fbf802b
lupera13k <kevinlupera1997@outlook.com>
2017-06-11 18:48:29 -0500

    Merge master into NN
    Conflicts:
    	codigos/footer.php
    	login.php
    	loginController.php

--- funciones/funciones.js
+++ funciones/funciones.js
@@ -9,7 +9,7 @@ function validaSoloNumeros() {
   event.returnValue = false;
 }
 function validaSoloLetras() {
- if ((event.keyCode != 32) && (event.keyCode < 65) || (event.keyCode > 90) && (event.keyCode < 97) || (event.keyCode > 122))
+ if ((event.keyCode !== 32) && (event.keyCode < 65) || (event.keyCode > 90) && (event.keyCode < 97) || (event.keyCode > 122))
     event.returnValue = false;
 }
 function validaLetrasNumeros() {
